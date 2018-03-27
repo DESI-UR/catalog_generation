27529cc33d86ff501eb36378b9560d434e933c1f
tolgayapici <tyapici@pa.msu.edu>
2018-03-27 11:50:31 -0400

    refactored the script. one part has the tools, the other uses the tools

--- py/generation_tools.py
+++ py/generation_tools.py
@@ -9,7 +9,7 @@ import numpy as np
 import healpy as hp
 import math, random
 import matplotlib as mpl
-mpl.use("Agg")
+#mpl.use("Agg")
 import matplotlib.pyplot as plt
 import configparser
 from astropy.io import fits
@@ -22,16 +22,24 @@ import os
 NSIDE = -1
 fbool = False
 
+DEG2RAD = np.pi/180.0
+RAD2DEG = 1./DEG2RAD
+
 class GeneralTools():
     
-    def __init__(self, configFile):
+    def __init__(self, configFile, diagnostics=False):
         """
         COMMENTS HERE
         """
+        self.diagnostics = diagnostics
         self.config_file = configFile
         self.getConfig()
         self.get_template()
         self.get_mask()
+
+
+    def setDiagnostics(self, diagnostics):
+        self.diagnostics = diagnostics
         
     def getConfig(self):
         self.config     = configparser.RawConfigParser()
@@ -59,6 +67,7 @@ class GeneralTools():
         self.r_BAO          = float(self.config.get('Gen Params','r_bao'))
         self.sigma_r_BAO    = float(self.config.get('Gen Params','sigr_bao'))
         self.gamma          = float(self.config.get('Gen Params','gamma'))
+        self.r_0            = float(self.config.get('Gen Params','r_0'))
         self.n_rnd          = int(self.config.get('Gen Params','n_rand'))
         self.n_center       = int(self.config.get('Gen Params','n_center'))
         self.n_rim          = int(self.config.get('Gen Params','n_rim'))
@@ -91,22 +100,20 @@ class GeneralTools():
         self.completeness     = self.mask.f.CMPLTNSS
         self.completeness_len = len(self.completeness)
         self.nside            = hp.npix2nside(len(self.completeness))
-        if diagnostics:
+        if diagnostics or self.diagnostics:
+            self.check_diagnostics_directory()
             plt.clf()
             hp.mollview(self.completeness, title="Completeness")
-            if not os.path.isdir("diagnostics"):
-                os.makedirs("diagnostics")
             plt.savefig("diagnostics/completeness.pdf")
 
     # generate r values uniformly distributed with (r_min, r_max)
     def generate_uniform_r(self, num_obs, diagnostics=False):
         flat_r = np.random.uniform(self.template_r_min, self.template_r_max, num_obs)
         # if diagnostics enabled, plot the distribution
-        if diagnostics:
+        if diagnostics or self.diagnostics:
+            self.check_diagnostics_directory()
             plt.hist(flat_r)
             plt.xlabel("r [Mpc]")
-            if not os.path.isdir("diagnostics"):
-                os.makedirs("diagnostics")
             plt.title("Flat r distribution")
             plt.savefig("diagnostics/r_distribution.pdf")        
         return flat_r
@@ -127,14 +134,17 @@ class GeneralTools():
             num_obs += 1
         rlist = np.array(rlist)
         # plot the distribution for diagnostics
-        if diagnostics:
+        if diagnostics or self.diagnostics:
+            self.check_diagnostics_directory()
             plt.hist(rlist)
             plt.xlabel("r [Mpc]")
-            if not os.path.isdir("diagnostics"):
-                os.makedirs("diagnostics")
             plt.savefig("diagnostics/r_distribution.pdf")
         return rlist
 
+    def check_diagnostics_directory(self):
+        if not os.path.isdir("diagnostics"):
+                os.makedirs("diagnostics")
+    
     def generate_uniform_angular_position(self, nobs, diagnostics=False):
         num_obs = 0
         pixlist = []
@@ -146,14 +156,13 @@ class GeneralTools():
             num_obs += 1
         angles = hp.pix2ang(ipix=np.array(pixlist), nside=self.nside, lonlat=True)
         # plot the distribution for diagnostics
-        if diagnostics:
+        if diagnostics or self.diagnostics:
             # completeness plot
             plt.clf()
             pixels = np.zeros([12*self.nside**2])
             pixels[pixlist] = 1
             hp.mollview(pixels.astype(int), title="Completeness")
-            if not os.path.isdir("diagnostics"):
-                os.makedirs("diagnostics")
+            self.check_diagnostics_directory()
             plt.savefig("diagnostics/generated_completeness.pdf")
             # theta distribution
             plt.clf()
@@ -190,23 +199,24 @@ class GeneralTools():
 
     # function to convert spherical coordinates to cartesian coordinates
     def toCartesianVector(self, r, theta, phi):
-        x = r * math.sin(theta) * math.cos(phi)
-        y = r * math.sin(theta) * math.sin(phi)
-        z = r * math.cos(theta)
-        return np.array([x, y, z])
-
+        unit_vector = hp.pix2vec(self.nside, hp.ang2pix(self.nside, theta, phi, lonlat=True))
+        curr_vector = r * np.array(unit_vector)
+        return curr_vector
+        
     # function to convert cartesian coordinates to spherical coordinates
     def fromCartesianVector(self, vec):
-        r     = np.sqrt( np.sum(np.array(vec)**2) )
-        theta = np.degrees(np.arctan(vec[1]/vec[0]))
-        phi   = np.degrees(np.arctan(vec[2]/vec[0]))
-        return np.array([r, theta, phi])
-
+        r           = np.sqrt( np.sum(np.array(vec)**2) )
+        unit_vector = hp.vec2ang(vec, lonlat=True)
+        return r, unit_vector[0], unit_vector[1]
+        
     # function to add two spherical vectors
     def addVectors(self, vec1, vec2):
-        vec1_cartesian = self.toCartesianVector(*vec1)
-        vec2_cartesian = self.toCartesianVector(*vec2)
-        return self.fromCartesianVector(vec1_cartesian+vec2_cartesian)
+        vec1_cartesian  = self.toCartesianVector(*vec1)
+        vec2_cartesian  = self.toCartesianVector(*vec2)
+        sumvec          = self.fromCartesianVector(vec1_cartesian+vec2_cartesian)
+        # this part is necessary to get the added vector to the right quadrant
+        # it is not intended for general use but specific to this project
+        return sumvec
 
     #
     def generate_rim(self, r, theta, phi):
@@ -215,19 +225,23 @@ class GeneralTools():
         rim_phis   = []
         len_n_center = len(r)
         for i in range(len_n_center):
-            curr_theta = np.random.uniform(0., 360., self.n_rim)
-            curr_phi   = np.random.uniform(0., 180., self.n_rim)
-            curr_r     = self.generate_gaussian(self.r_BAO, self.sigma_r_BAO, self.n_rim)
-            for j in range(self.n_rim):
+            curr_rim_cnt = 0
+            while curr_rim_cnt < self.n_rim:
+                curr_theta = np.random.uniform(0., 360., 1)
+                curr_phi   = np.random.uniform(-90., 90., 1)
+                curr_r     = self.generate_gaussian(self.r_BAO, self.sigma_r_BAO, 1)
                 curr_rim   = self.addVectors(np.array([r[i], theta[i], phi[i]]),
-                                             np.array([curr_r[j], curr_theta[j], curr_phi[j]]))
-                rim_rs.append(curr_rs)
-                rim_thetas.append(curr_thetas)
-                rim_phis.append(curr_phis)    
+                                             np.array([curr_r[0], curr_theta[0], curr_phi[0]]))
+                pixel      = hp.ang2pix(self.nside, curr_rim[1], curr_rim[2], lonlat=True)
+                # this is the acceptance.
+                if self.completeness[pixel] > 0:
+                    rim_rs.append(curr_rim[0])
+                    rim_thetas.append(curr_rim[1])
+                    rim_phis.append(curr_rim[2])
+                    curr_rim_cnt += 1
         rim_rs = np.array(rim_rs).flatten()
         rim_thetas = np.array(rim_thetas).flatten()
         rim_phis = np.array(rim_phis).flatten()
-        # need to add acceptance for the rim galaxies
         return rim_rs, rim_thetas, rim_phis
 
     def generate_clumps(self, r_centers, theta_centers, phi_centers, diagnostics=False):
@@ -237,97 +251,21 @@ class GeneralTools():
         rand_center_idx = np.random.randint(0, len(r_centers), self.n_clump_center)
         rand_flat_idx   = np.random.randint(0, len(r_flat),    self.n_clump)
         # generate the clump positions with respect to their origin (a center galaxu)
-        clump__r       = (self.r_0**self.gamma * (np.random.pareto(self.gamma-1, self.n_clump_center)))
+        clump_r        = (self.r_0**self.gamma * (np.random.pareto(self.gamma-1, self.n_clump_center)))
         clump_theta    = np.random.uniform(0., 360., self.n_clump_center)
-        clump_phi      = np.random.uniform(0., 180., self.n_clump_center)
-        center_clumps  = self.addVectors(np.array([r_centers[rand_center_idx], theta_centers[rand_center_idx], phi_centers[rand_center_idx]]),
-                                              np.array([clump_r, clump_theta, clump_phi]))
+        clump_phi      = np.random.uniform(-90., 90., self.n_clump_center)
+        center_clumps  = []
+        for i in range(self.n_clump_center):
+            center_clumps.append(self.addVectors(np.array([r_centers[rand_center_idx][i], theta_centers[rand_center_idx][i], phi_centers[rand_center_idx][i]]),
+                                                 np.array([clump_r[i], clump_theta[i], clump_phi[i]])))
         # generate the clump positions with respect to their origin (a flat galaxu)
         clump_r        = (self.r_0**self.gamma * (np.random.pareto(self.gamma-1, self.n_clump)))
         clump_theta    = np.random.uniform(0., 360., self.n_clump)
-        clump_phi      = np.random.uniform(0., 180., self.n_clump)
-        flat_clumps    = self.addVectors(np.array([r_flat[rand_flat_idx], theta_flat[rand_flat_idx], phi_flat[rand_flat_idx]]),
-                                         np.array([clump_r, clump_theta, clump_phi]))
-        # merge two arrays
-        clump_rs     = np.append(center_clumps[0], flat_clumps[0])
-        clump_thetas = np.append(center_clumps[1], flat_clumps[1])
-        clump_phis   = np.append(center_clumps[2], flat_clumps[2])
-        #
-        return clump_rs, clump_thetas, clump_phis, f_flat, theta_flat, phi_flat
+        clump_phi      = np.random.uniform(-90., 90., self.n_clump)
+        flat_clumps    = []
+        for i in range(self.n_clump):
+            flat_clumps.append(self.addVectors(np.array([r_flat[rand_flat_idx][i], theta_flat[rand_flat_idx][i], phi_flat[rand_flat_idx][i]]),
+                                               np.array([clump_r[i], clump_theta[i], clump_phi[i]])))
         
-    #generate random catalog with data distribution in r,phi,theta
-    def generate_random(self, catalog_no=0):
-        try:
-            from ROOT import Galaxy
-        except:
-            self.genROOTObject()
-            from ROOT import Galaxy
-        tfilename   = self.fname_random+"_"+str(catalog_no)+".root"
-        output      = TFile(tfilename, "RECREATE")
-        output_tree = TTree("data_pol", "data_pol")
-        galaxy      = Galaxy()
-        if self.coordinates == 0:
-            output_tree.Branch("position_pol", galaxy, "phi/F:theta/F:z/F:weight/F")
-        else:
-            output_tree.Branch("position_pol", galaxy, "ra/F:dec/F:z/F:weight/F")
-        rs, thetas, phis = self.generate_galaxies(self.n_rnd)
-        zs               = [z_at_value(self.cosmo.comoving_distance, r*u.Mpc) for r in rs]
-        weights          = np.array([1.]*len(rs))  # THIS SEEMS WRONG
-        galaxy  = Galaxy()
-        ras     = phis*(180./np.pi)
-        decs    = ((np.pi/2.)-thetas)*(180./np.pi)
-        for i in range(self.n_rnd):
-            galaxy.z = zs[i]
-            if self.coordinates == 0:
-                galaxy.phi = phis[i]
-                galaxy.theta = thetas[i]
-                galaxy.w = weights[i]
-            else:
-                galaxy.ra = ras[i]
-                galaxy.dec = decs[i]
-                galaxy.weight = weights[i]
-            output_tree.Fill()
-        output.Write()
-        output.Close()
-        fits_filename = self.fname_random+"_"+str(ncat)+".fits"
-        if self.coordinates == 0:
-            self.write_to_fits(phis, thetas, zs, weights, fits_filename)
-        else:
-            self.write_to_fits(ras, decs, zs, weights, fits_filename)
-
-    def generate_mock(self, catalog_no=0):
-        try:
-            from ROOT import Galaxy
-        except:
-            self.genROOTObject()
-            from ROOT import Galaxy
-        tfilename   = self.fname_random+"_"+str(catalog_no)+".root"
-        output      = TFile(tfilename, "RECREATE")
-        output_tree = TTree("data_pol", "data_pol")
-        galaxy      = Galaxy()
-        if self.coordinates == 0:
-            output_tree.Branch("position_pol", galaxy, "phi/F:theta/F:z/F:weight/F")
-        else:
-            output_tree.Branch("position_pol", galaxy, "ra/F:dec/F:z/F:weight/F")
-        r_centers, theta_centers, phi_centers = self.generate_galaxies(self.n_center)
-        # TODO:
-        # add flats
-        # add rims
-        # add clumps
-            
-    def write_to_fits(self, col1, col2, col3, col4, filename):
-        col_defs = [['phi' 'theta', 'z', 'weight'], ['ra', 'dec', 'z', 'weight']]
-        print(self.coordinates)
-        use_col_defs = col_defs[self.coordinates]
-        # We also write the output in fits format
-        if os.path.isfile(filename):
-            print("a file with the designated name already exists... please remove the file first")
-            return
-        col1 = fits.Column(name=use_col_defs[0], array=col1, format='f8')
-        col2 = fits.Column(name=use_col_defs[1], array=col2, format='f8')
-        col3 = fits.Column(name=use_col_defs[2], array=col3,  format='f8')
-        col4 = fits.Column(name=use_col_defs[3], array=col4,  format='f8')
-        cols = fits.ColDefs([col1, col2, col3, col4])
-        hdu  = fits.BinTableHDU.from_columns(cols)
-        hdu.writeto(filename)
-
+        return (np.array(center_clumps)).transpose(), (np.array(flat_clumps)).transpose(), np.array([r_flat, theta_flat, phi_flat])
+        
