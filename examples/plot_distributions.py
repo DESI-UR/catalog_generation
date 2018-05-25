a0a317cf3768e9ab5746f9d506f7b254a7adb008
tolgayapici <tyapici@pa.msu.edu>
2018-05-25 10:04:51 -0400

    some problem making lines are commented, to be fixed later

--- examples/plot_distributions.py
+++ examples/plot_distributions.py
@@ -1,9 +1,3 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
 import sys
 sys.path.append("../py/")
 sys.path.append("..")
@@ -68,6 +62,7 @@ ax.hist(center_clumps['ra'], alpha=0.3, normed=True)
 ax.hist(flat_clumps['ra'], alpha=0.3, normed=True)
 ax.hist(flats['ra'], alpha=0.3, normed=True)
 ax.set_xlabel(r'RA [deg]')
+ax.set_xlim(0, 60)
 ax = plt.subplot(246)
 ax.hist(centers['dec'], alpha=0.7, label="center galaxies", normed=True)
 try:
@@ -81,13 +76,13 @@ box = ax.get_position()
 ax.set_position([box.x0, box.y0, box.width , box.height])
 ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 ax.set_xlabel(r'DEC [deg]')
-ax.text(600, 0.0040, r'{}'.format(sys.argv[1].replace(".fits", "")), horizontalalignment='right', fontweight='bold')
-ax.text(600, 0.0035, r'N$_{{center}}$: {}'.format(len(centers)), horizontalalignment='right')
-ax.text(600, 0.0030, r'N$_{{rim}}$: {}'.format(len(rims)), horizontalalignment='right')
-ax.text(600, 0.0025, r'N$_{{flat}}$: {}'.format(len(flats)), horizontalalignment='right')
-ax.text(600, 0.0020, r'N$_{{center clump}}$: {}'.format(len(center_clumps)), horizontalalignment='right')
-ax.text(600, 0.0015, r'N$_{{flat clump}}$: {}'.format(len(flat_clumps)), horizontalalignment='right')
-ax.text(600, 0.0010, r'N$_{{total}}$: {}'.format(len(centers)+len(rims)+len(flats)+len(center_clumps)+len(flat_clumps)), horizontalalignment='right')
+#ax.text(600, 0.0040, r'{}'.format(sys.argv[1].replace(".fits", "")), horizontalalignment='right', fontweight='bold')
+#ax.text(600, 0.0035, r'N$_{{center}}$: {}'.format(len(centers)), horizontalalignment='right')
+#ax.text(600, 0.0030, r'N$_{{rim}}$: {}'.format(len(rims)), horizontalalignment='right')
+#ax.text(600, 0.0025, r'N$_{{flat}}$: {}'.format(len(flats)), horizontalalignment='right')
+#ax.text(600, 0.0020, r'N$_{{center clump}}$: {}'.format(len(center_clumps)), horizontalalignment='right')
+#ax.text(600, 0.0015, r'N$_{{flat clump}}$: {}'.format(len(flat_clumps)), horizontalalignment='right')
+#ax.text(600, 0.0010, r'N$_{{total}}$: {}'.format(len(centers)+len(rims)+len(flats)+len(center_clumps)+len(flat_clumps)), horizontalalignment='right')
         
 filename = "distributions_" + sys.argv[1].replace(".fits", ".pdf")
 plt.savefig(filename, bbox_inches='tight')
