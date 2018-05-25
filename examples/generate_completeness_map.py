bdf45cf0507abfaf20bc8823e7f933dd4b355174
tolgayapici <tyapici@pa.msu.edu>
2018-05-25 09:58:19 -0400

    a script is added to generate custom completeness maps

---
+++ examples/generate_completeness_map.py
@@ -0,0 +1,44 @@
+
+import healpy as hp
+import numpy as np
+import sys
+import matplotlib.pyplot as plt
+import argparse
+
+RAD2DEG    = 180./np.pi
+DEG2RAD    = 1/RAD2DEG
+
+parser = argparse.ArgumentParser(description="This script generates a completeness map based on "+\
+                                 "the given RA and Dec limits")
+parser.add_argument("-r", "--ra",     type=float, help="RA limits",  nargs=2, required=True)
+parser.add_argument("-d", "--dec",    type=float, help="Dec limits", nargs=2, required=True)
+parser.add_argument("-n", "--nside",  type=int,   help="nside for the generated cocmpleteness", default=1024)
+parser.add_argument("-o", "--output", type=str,   help="output filename", required=True)
+parser.add_argument("-p", "--plot",   type=int,   help="plot the resukts in mollview", default=0)
+args   = parser.parse_args()
+
+ra_min = np.min(args.ra)
+ra_max = np.max(args.ra)
+
+dec_min = np.min(args.dec)
+dec_max = np.max(args.dec)
+
+nside      = args.nside
+resolution = hp.nside2resol(nside)*RAD2DEG
+
+decs   = np.arange(dec_min, dec_max, resolution)
+ras    = np.arange(ra_min,  ra_max, resolution)
+pixels = np.zeros(hp.nside2npix(nside))
+
+for ra in ras:
+    for dec in decs:
+        pixels[hp.ang2pix(nside, ra-90., dec, lonlat=True)] = 1
+pixels = np.array(pixels)
+
+if args.plot:
+    hp.mollview(pixels, cbar=False, title='Completeness')
+    plt.savefig("completeness.pdf")
+    plt.show()
+    
+np.savez(args.output, CMPLTNSS=pixels)
+
