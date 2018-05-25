1108e7d5844433bc48ceb8386c46c64a8613f3cc
tolgayapici <tyapici@pa.msu.edu>
2018-05-25 10:03:10 -0400

    commented debugging lines were removed from mock generation example

--- examples/mock_catalog_generation.py
+++ examples/mock_catalog_generation.py
@@ -46,6 +46,7 @@ print("Number of rim galaxies: {}".format(len(r_rim)))
 # are defined in the configuration file.
 
 center_clumps, flat_clumps, flats = gt.generate_clumps(np.append(r_center, r_rim), np.append(theta_center, theta_rim), np.append(phi_center, phi_rim))
+#center_clumps, flat_clumps, flats = gt.generate_clumps(r_center, theta_center, phi_center)
 print("Number of flat galaxies: {}".format(len(flats[0])))
 print("Number of center clump galaxies: {}".format(len(center_clumps[0])))
 print("Number of flat clump galaxies: {}".format(len(flat_clumps[0])))
@@ -56,7 +57,7 @@ print("Number of flat clump galaxies: {}".format(len(flat_clumps[0])))
 r2z_function = gt.generate_LUT_r2z()
 # first, the center galaxies:
 z_center     = r2z_function(r_center)
-z_acceptance = z_center<=gt.z_max #gt.check_z_acceptance(z_center)
+z_acceptance = z_center<=gt.z_max
 z_center     = z_center[z_acceptance]
 theta_center = theta_center[z_acceptance]
 phi_center   = phi_center[z_acceptance]
@@ -68,7 +69,7 @@ theta_rim = theta_rim[z_acceptance]
 phi_rim   = phi_rim[z_acceptance]
 # third, the flat galaxies
 z_flat       = r2z_function(flats[0])
-z_acceptance = z_flat<=gt.z_max#gt.check_z_acceptance(z_flat)
+z_acceptance = z_flat<=gt.z_max
 z_flat       = z_flat[z_acceptance]
 theta_flat   = flats[1][z_acceptance]
 phi_flat     = flats[2][z_acceptance]
@@ -87,104 +88,6 @@ phi_flat_clumps   = flat_clumps[2][z_acceptance]
 # lastly, we duplicate the z-template and apply acceptance test for diagnostic purposes
 template_z   = np.array(gt.template_z)
 template_z   = template_z[template_z<gt.z_max]
-
-"""
-# The results
-# The distributions and the positions of the galaxies are plotted here. As can be seen, the acceptance is applied during the generation.
-plt.figure(1, figsize=(16,8))
-
-plt.subplot(241)
-plt.hist(template_z, bins=int(np.sqrt(len(template_z))), alpha=0.3, normed=True, label="Template")
-plt.hist(z_center, bins=int(np.sqrt(len(z_center))), alpha=0.3, normed=True, label="BAO Centers")
-plt.legend(loc=2)
-plt.xlabel("z [redshift]")
-
-plt.subplot(242)
-plt.hist(template_z, bins=int(np.sqrt(len(template_z))), alpha=0.3, normed=True, label="Template")
-plt.hist(z_rim, bins=int(np.sqrt(len(z_rim))), alpha=0.3, normed=True, label="BAO rims")
-plt.legend(loc=2)
-plt.xlabel("z [redshift]")
-
-plt.subplot(243)
-plt.hist(template_z, bins=int(np.sqrt(len(template_z))), alpha=0.3, normed=True, label="Template")
-plt.hist(z_center_clumps, bins=int(np.sqrt(len(z_center_clumps))), alpha=0.3, normed=True, label="Center clumps")
-plt.legend(loc=2)
-plt.xlabel("z [redshift]")
-
-plt.subplot(244)
-plt.hist(template_z, bins=int(np.sqrt(len(template_z))), alpha=0.3, normed=True, label="Template")
-plt.hist(z_flat_clumps, bins=int(np.sqrt(len(z_flat_clumps))), alpha=0.3, normed=True, label="Flat clumps")
-plt.legend(loc=2)
-plt.xlabel("z [redshift]")
-
-plt.subplot(245)
-plt.hist(template_z, bins=int(np.sqrt(len(template_z))), alpha=0.3, normed=True, label="Template")
-plt.hist(z_flat, bins=int(np.sqrt(len(z_flat))), alpha=0.3, normed=True, label="Flats")
-plt.legend(loc=2)
-plt.xlabel("z [redshift]")
-
-ax = plt.subplot(246)
-ax.hist(theta_center, alpha=0.3, normed=True)
-ax.hist(theta_rim, alpha=0.3, normed=True)
-ax.hist(theta_center_clumps, alpha=0.3, normed=True)
-ax.hist(theta_flat_clumps, alpha=0.3, normed=True)
-ax.hist(theta_flat, alpha=0.3, normed=True)
-ax.set_xlabel(r'$\theta$ [deg]')
-
-ax = plt.subplot(247)
-ax.hist(phi_center, alpha=0.7, label="center galaxies", normed=True)
-ax.hist(phi_rim, alpha=0.3, label="rim galaxies", normed=True)
-ax.hist(phi_center_clumps, alpha=0.3, label="center clumps", normed=True)
-ax.hist(phi_flat_clumps, alpha=0.3, label="flat clumps", normed=True)
-ax.hist(phi_flat, alpha=0.3, label="flat galaxies", normed=True)
-box = ax.get_position()
-ax.set_position([box.x0, box.y0, box.width, box.height])
-ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-ax.set_xlabel(r'$\phi$ [deg]')
-plt.savefig("distributions.pdf", bbox_inches='tight')
-plt.show()
-"""
-"""
-plt.figure(2, figsize=(12,12))
-plt.subplot(221)
-plt.plot(theta_center, phi_center, 'o', alpha=0.2, label="center galaxies")
-plt.plot(theta_rim, phi_rim, 'o', alpha=0.2, label="rim galaxies")
-plt.xlim(0, 360)
-plt.ylim(-90, 90)
-plt.xlabel(r"$\theta$ [deg]")
-plt.ylabel(r"$\phi$ [deg]")
-plt.legend()
-plt.grid(True)
-plt.subplot(222)
-plt.plot(theta_center, phi_center, 'o', alpha=0.2, label="center galaxies")
-plt.plot(center_clumps[1], center_clumps[2], 'o', alpha=0.2, label="central clumps")
-plt.xlim(0, 360)
-plt.ylim(-90, 90)
-plt.xlabel(r"$\theta$ [deg]")
-plt.ylabel(r"$\phi$ [deg]")
-plt.legend()
-plt.grid(True)
-plt.subplot(223)
-plt.plot(theta_center, phi_center, 'o', alpha=0.2, label="center galaxies")
-plt.plot(flat_clumps[1], flat_clumps[2], 'o', alpha=0.2, label="flat clumps")
-plt.xlim(0, 360)
-plt.ylim(-90, 90)
-plt.xlabel(r"$\theta$ [deg]")
-plt.ylabel(r"$\phi$ [deg]")
-plt.legend()
-plt.grid(True)
-plt.subplot(224)
-plt.plot(theta_center, phi_center, 'o', alpha=0.2, label="center galaxies")
-plt.plot(flats[1], flats[2], 'o', alpha=0.2, label="flat galaxies")
-plt.xlim(0, 360)
-plt.ylim(-90, 90)
-plt.xlabel(r"$\theta$ [deg]")
-plt.ylabel(r"$\phi$ [deg]")
-plt.legend()
-plt.grid(True)
-plt.savefig("maps.pdf")
-plt.show()
-"""
 
 all_zs     = np.append(z_center, np.append(z_rim, np.append(z_center_clumps, np.append(z_flat_clumps, z_flat))))
 all_thetas = np.append(theta_center, np.append(theta_rim, np.append(theta_center_clumps, np.append(theta_flat_clumps, theta_flat))))
