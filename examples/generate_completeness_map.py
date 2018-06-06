8bb0da22ea3a3bc97c2a8be972f2948b615165ef
tolgayapici <tyapici@pa.msu.edu>
2018-06-06 13:05:20 -0400

    fixed the coordinate problem on completeness generation

--- examples/generate_completeness_map.py
+++ examples/generate_completeness_map.py
@@ -11,7 +11,7 @@ DEG2RAD    = 1/RAD2DEG
 parser = argparse.ArgumentParser(description="This script generates a completeness map based on "+\
                                  "the given RA and Dec limits")
 parser.add_argument("-r", "--ra",     type=float, nargs=2, required=True, help="RA limits  (range: [0, 360])")
-parser.add_argument("-d", "--dec",    type=float, nargs=2, required=True, help="Dec limits (range: [0, 180])")
+parser.add_argument("-d", "--dec",    type=float, nargs=2, required=True, help="Dec limits (range: [-90, 90])")
 parser.add_argument("-n", "--nside",  type=int,   help="nside for the generated cocmpleteness", default=1024)
 parser.add_argument("-o", "--output", type=str,   help="output filename", required=True)
 parser.add_argument("-p", "--plot",   type=int,   help="plot the resukts in mollview", default=0)
@@ -32,7 +32,7 @@ pixels = np.zeros(hp.nside2npix(nside))
 
 for ra in ras:
     for dec in decs:
-        pixels[hp.ang2pix(nside, 360.-ra, 90.-dec, lonlat=True)] = 1
+        pixels[hp.ang2pix(nside, 90.-ra, -dec, lonlat=True)] = 1
 pixels = np.array(pixels)
 
 if args.plot:
