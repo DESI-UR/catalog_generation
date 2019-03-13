4b1421dede4aa22b10c8878f29dabd200e72f2ce
tolgayapici <tyapici@pa.msu.edu>
2019-03-13 14:23:14 -0400

    the executable has not argument parser and more user friendly

--- bin/generate_mock.py
+++ bin/generate_mock.py
@@ -1,16 +1,49 @@
 #!/usr/bin/env python
 
-import sys
+import argparse
 
 from paramock.paramock import GeneralTools as generalTools
-print("import complete")
+
+parser = argparse.ArgumentParser(description="")
+parser.add_argument("-c", "--config",
+                    type=str,
+                    help="Configuration filename",
+                    required=True)
+parser.add_argument("-o", "--output",
+                    type=str,
+                    help="Output filename (this overwrites the output filename in the configuration file",
+                    required=False,
+                    default=None)
+parser.add_argument("-d", "--diagnostics",
+                    type=bool,
+                    help="Flag to get diagnostic messages",
+                    required=False,
+                    default=False)
+parser.add_argument("-a", "--add_to_rims",
+                    type=bool,
+                    help="Flag to add clumping galaxies around rim galaxies",
+                    required=False,
+                    default=False)
+parser.add_argument("-e", "--extended",
+                    type=bool,
+                    help="Flag to write the extended output for the fits file",
+                    required=False,
+                    default=False)
+parser.add_argument("-p", "--pickle",
+                    type=bool, help="Flag to write the extended pickle file",
+                    required=False,
+                    default=False)
+parser.add_argument("-u", "--use_style",
+                    type=bool, help="Flag to use the provided plotting style",
+                    required=False,
+                    default=True)
+args   = parser.parse_args()
 
 # Generating Mock Catalogs
-# This example shows the use of individual components of the mock generation tools. Each step is explained followed
-# by the use of the specific function. Here we use a configuration file which is similar to the original "../data/catget.cfg".
-# The only difference is the pathing for the input files in the configuration file. In the future, we will try to remove the hardcoded
-# paths from the configuration file.
-gt = generalTools(sys.argv[1], diagnostics=False)
+
+# Instantiate the module with the user defined configuration file
+# Detailed information about individual methids are given below as well
+gt = generalTools(args.config, args.diagnostics, use_style=args.use_style)
 
 # Generating central galaxies
 # The function (generate_galaxies) is used to generate the galaxies in the mock catalog. User can choose either to use a
@@ -31,14 +64,18 @@ gt.generate_rim()
 # For generating the clumps, the user has to provide the center galaxy coordinates. These coordinates are
 # used in the code. Also in the code, flat galaxies are generated and used for injecting clumps. All the input parameters
 # are defined in the configuration file.
-gt.generate_clumps(add_clumps_to_rims=True, diagnostics=True)
+gt.generate_clumps(args.add_to_rims)
 
 # Save the output
 # there are two options for user. One output file is in fits format to be used with other TPCF codes
 # the other output is in pickle format and it stores detailed information about the mock catalog generated
-gt.write_to_fits()
+gt.write_to_fits(filename=args.output, save_extended=args.extended)
 
 # If the user want to store everything is the catalog structure (galaxies with their corresponding rims, clumps and all)
 # the user can uncomment the following line. Be cautious that this requires considerable memory and it may fail if used on
 # simple laptop or desktop computer
-#gt.write_to_pickle()
+if args.pickle:
+    if args.output is not None:
+        gt.write_to_pickle((args.output).replace(".fits", ".pkl"))
+    else:
+        gt.write_to_pickle()
