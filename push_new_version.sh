3ef22da1781ef1743b791feeb115cd16a4b1d7e3
Travis CI <travis@travis-ci.org>
2019-03-15 09:29:50 -0400

    hopefully the last changes for the automatic version updating

--- push_new_version.sh
+++ push_new_version.sh
@@ -2,7 +2,8 @@
 
 python_version=`python -c "import sys; print(\"{}.{}\".format(sys.version_info[0], sys.version_info[1]))"`
 
-if [ $python_version == "3.6" ]; then 
+if [ $python_version == "3.6" ]
+then 
     echo "Updating the version file for python $python_version only"
     git config --global user.email "travis@travis-ci.org"
     git config --global user.name "Travis CI"
