8acb8108c32e0482508e22854f173c04998aef66
Travis CI <travis@travis-ci.org>
2019-03-25 13:21:55 -0400

    [skip travis] updated the setup file for the redundant requirement statements

--- setup.py
+++ setup.py
@@ -45,7 +45,6 @@ print("Version is set to {}".format(ver.get_version(out_type='string')))
 #
 # END OF SETTINGS THAT NEED TO BE CHANGED.
 #
-#setup_keywords['version'] = get_version(setup_keywords['name'])
 
 # Set other keywords for the setup function.  These are automated, & should
 # be left alone unless you are an expert.
@@ -57,8 +56,6 @@ if os.path.isdir('bin'):
         if not os.path.basename(fname).endswith('.rst')]
 
 setup_keywords['provides'] = [setup_keywords['name']]
-setup_keywords['requires'] = ['Python (>3.5)', 'healpy (>=1.11.0)', 'numpy (>=1.13.1)', 'configparser (>=3.5)', \
-                              'astropy (>=1.2.1)', 'scipy (>=0.19.1)', 'matplotlib (>=2.0.0)']
 setup_keywords['install_requires'] = ['healpy>=1.11.0', 'numpy>=1.13.1', 'configparser>=3.5', \
                                       'astropy>=1.2.1', 'scipy>=0.19.1', 'matplotlib>=2.0.0']
 setup_keywords['zip_safe'] = False
@@ -67,18 +64,10 @@ setup_keywords['packages'] = find_packages('py')
 setup_keywords['package_dir'] = {'':'py'}
 setup_keywords['cmdclass'] = {'install': Install,}
 
-#setup_keywords['cmdclass'] = {'version': DesiVersion, 'test': DesiTest, 'sdist': DistutilsSdist}
-#setup_keywords['test_suite']='{name}.test.test_suite'.format(**setup_keywords)
-
-#
-# Autogenerate command-line scripts.
-#
-# setup_keywords['entry_points'] = {'console_scripts':['desiInstall = desiutil.install.main:main']}
-#
 # Add internal data directories.
 #
 setup_keywords['package_data'] = {'ParaMock': ['data/*',]}
-#
+
 # Run setup command.
 #
 setup(**setup_keywords)
